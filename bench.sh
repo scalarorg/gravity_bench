7be26bad61a69a8653c98905ebd6a60c60e6f65f
Vu Viet Tai <vuviettai@gmail.com>
2026-01-22 14:32:25 +0700

    Update bench.sh script

--- bench.sh
+++ bench.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# source ./setup.sh
-cargo run --bin gravity_bench --release -- --config bench_config_fastevm.toml --log-level info
+
+source ./setup.sh
+RUST_LOG=debug cargo run --bin gravity_bench --release -- --config bench_config_fastevm.toml
